382b7152cef224fc6d7a9b1c7b9ed4c3fdc08af1
maxime <maxime.aumegeas@laplateforme.io>
2025-12-09 14:07:27 +0100

    Modif

--- src/main/java/com/maxime/Exo/thymeleaf/repository/ArticleRepository.java
+++ src/main/java/com/maxime/Exo/thymeleaf/repository/ArticleRepository.java
@@ -2,9 +2,11 @@ package com.maxime.Exo.thymeleaf.repository;
 
 import com.maxime.Exo.thymeleaf.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article,Long> {
     List<Article> findbyauthor(String author);
 }
--- src/main/java/com/maxime/Exo/thymeleaf/services/ArticleServices.java
+++ src/main/java/com/maxime/Exo/thymeleaf/services/ArticleServices.java
@@ -1,14 +1,18 @@
 package com.maxime.Exo.thymeleaf.services;
 
 import com.maxime.Exo.thymeleaf.model.Article;
+import com.maxime.Exo.thymeleaf.repository.ArticleRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ArticleServices {
-
+    private ArticleRepository repository;
     public List<Article> getListArticle(){
         List<Article> listArticle = new ArrayList<>();
         listArticle.add(new Article(1L,"allo win", "Mirette","SuperTomate"));
@@ -17,4 +21,32 @@ public class ArticleServices {
         listArticle.add(new Article(4L,"azerty", "Clavier","AZERTY"));
         return listArticle;
     }
+    public Article findArticleById (Long id){
+        return repository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("Software Engineerwith id " + id + " not found"));
+    }
+    public void createNewArticle(Article article) {
+        repository.save(article);
+    }
+    public void modifyArticle(Long id, Map<String, Object> update) {
+        Article articleUpdate = findArticleById(id);
+        update.forEach((key , value)->{
+            Field filed = ReflectionUtils.findField(Article.class , key);
+            if (filed != null){
+                filed.setAccessible(true);
+                ReflectionUtils.setField(filed,articleUpdate,value);
+            }
+        });
+        repository.save(articleUpdate);
+    }
+    public void deleteArticle(Long id){
+        Article deleteArticle = findArticleById(id);
+        repository.delete(deleteArticle);
+    }
+    public void findArticleByAuthor(String author){
+        repository.findbyauthor(author);
+    }
+
+
+
 }
